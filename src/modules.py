c048cf7733b51b0991e7dbce207ad10d1987c420
h1n05aur <torrentlaugh@protonmail.com>
2021-06-08 15:09:33 +1000

    src dir

--- src/modules.py
+++ src/modules.py
@@ -7,13 +7,11 @@ import imgbbpy
 import discord
 import platform
 import textwrap
-import PIL.Image
-from PIL import Image
 from asyncio import sleep
 from bs4 import BeautifulSoup
 from discord.utils import get
 from pygicord import Paginator
 from dotenv import load_dotenv
 from discord.ext import commands
-from PillowSheet import Spreadsheet
+from src.PillowSheet import Spreadsheet
 from arsenic import get_session, keys, browsers, services
