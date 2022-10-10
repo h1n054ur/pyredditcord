db8897af394aaf11d8f078530fe8e4467244cfd3
h1n05aur <torrentlaugh@protonmail.com>
2022-10-10 18:48:52 +1100

    update

--- src/modules.py
+++ src/modules.py
@@ -7,8 +7,11 @@ import imgbbpy
 import discord
 import platform
 import textwrap
+import asyncio
 from asyncio import sleep
 from bs4 import BeautifulSoup
+from discord.ext import tasks
+from disputils import BotEmbedPaginator
 from discord.utils import get
 from pygicord import Paginator
 from dotenv import load_dotenv
